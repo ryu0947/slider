b441bf0936d9e141bc2e732e37be0a33faffe753
ryu <dragonogawaz@gmail.com>
2021-03-23 20:56:14 +0900

    スライダーの動きに合わせてインジゲーターを前後させる

--- js/slider1.js
+++ js/slider1.js
@@ -10,6 +10,7 @@ const images = [
 ];
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
+const indicator = document.querySelectorAll(".indicator li");
 
 let currentNum = 0;
 
@@ -17,9 +18,13 @@ next.addEventListener("click", () => {
   if (currentNum < images.length - 1) {
     ++currentNum;
     slideImg.src = images[currentNum];
+    indicator[currentNum - 1].classList.remove("current");
+    indicator[currentNum].classList.add("current");
   } else {
     currentNum = 0;
     slideImg.src = images[currentNum];
+    indicator[currentNum + images.length - 1].classList.remove("current");
+    indicator[currentNum].classList.add("current");
   }
 });
 
@@ -27,8 +32,12 @@ prev.addEventListener("click", () => {
   if (currentNum > 0) {
     currentNum--;
     slideImg.src = images[currentNum];
+    indicator[currentNum + 1].classList.remove("current");
+    indicator[currentNum].classList.add("current");
   } else {
     currentNum = images.length - 1;
     slideImg.src = images[currentNum];
+    indicator[0].classList.remove("current");
+    indicator[currentNum].classList.add("current");
   }
 });
