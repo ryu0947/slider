51eb0d0712a3095118fe008024901d92b4b8c530
ryu <dragonogawaz@gmail.com>
2021-03-24 07:13:46 +0900

    必要な要素を変数に格納

---
+++ js/slider2.js
@@ -0,0 +1,15 @@
+const slideImg = $("#slide-img");
+const images = [
+  "img/img1.jpg",
+  "img/img2.jpg",
+  "img/img3.jpg",
+  "img/img4.jpg",
+  "img/img5.jpg",
+];
+const next = $("#next");
+const prev = $("#prev");
+const indicator = $(".indicator li");
+
+let currentNum = 0;
+
+
